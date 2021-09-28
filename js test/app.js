9540eae9cac7d6c33924575b784644674d33282a
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2021-09-28 12:44:34 -0300

    seguimos con el DOM

--- js test/app.js
+++ js test/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-document.getElementsByTagName('h1')[0].innerText = 'Cambiando desde la DOM'
+document.getElementsByTagName('h1')[0].innerText = 'Cambiado desde la DOM'
 let fuenteroja = document.querySelectorAll('p.redfont')
 console.log(fuenteroja)
 
@@ -15,3 +15,30 @@ function show() {
     let text2 = nombre.value + ' ' + apellido.value
     document.getElementById('muestra').innerHTML = `Muestra por for loop: ${text}<br>Muestra por ingreso a forms[]: ${text2}` 
 }
+
+document.write("document.write")
+function overwrite(){
+    document.write('Sobreescribiendo la pagina')
+}
+
+function imageChange(){
+    document.getElementById('imagen').src = 'http://globalmentoring.com.mx/imagenes/html5.png'
+}
+
+function pColorChange(){
+    let cantRedFont = document.querySelectorAll('p.redfont')
+    for (let i = 0; i < cantRedFont.length; i++) {
+       cantRedFont[i].style.color = 'blue'
+        
+    }
+
+}
+
+function cambiarText(texto) {
+    texto.innerHTML = "Me clickeaste y cambie"
+}
+
+document.querySelectorAll('p.redfont')[1].onclick = cambiarTexto
+function cambiarTexto() {
+    this.innerHTML = "Cambio cambio cambio cambio"
+  }
