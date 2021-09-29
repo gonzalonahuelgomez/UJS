5b15181567cc51011c9397e689bbe3c9c52cd5a5
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2021-09-29 10:03:42 -0300

    empezando la calculadora

--- js test/js/app.js
+++ js test/js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-document.getElementsByTagName('h1')[0].innerText = 'Cambiado desde la DOM'
+document.getElementsByTagName('h1')[0].innerText = 'Cambiado desde la DOM, a que no me clickeas'
 let fuenteroja = document.querySelectorAll('p.redfont')
 console.log(fuenteroja)
 
@@ -42,3 +42,38 @@ document.querySelectorAll('p.redfont')[1].onclick = cambiarTexto
 function cambiarTexto() {
     this.innerHTML = "Cambio cambio cambio cambio"
   }
+
+function cookiesEnabled(){
+    let texto = ""
+    if(navigator.cookieEnabled) texto = "Cookies enabled"
+    else texto = "Cookies disabled"
+    document.getElementById('cookies').innerHTML = texto 
+}
+
+function changeToUpper(input){
+    input.value = input.value.toUpperCase()
+}
+
+function changeColor(e){
+    e.style.color = 'green'
+}
+function returnColor(e){
+    e.style.removeProperty('color')
+}
+
+function maintainClick(e){
+    e.innerHTML = "Solta"
+    e.style.color = 'purple'
+}
+
+function dropClick(e){
+    e.innerHTML = "Manteneme clickeado"
+    e.removeAttribute('style')
+}
+
+document.getElementById('eventListTest').addEventListener('focus', cambiar)
+document.getElementById('eventListTest').addEventListener('blur', (e)=>{e.target.style.background = "white"})
+
+function cambiar(e){
+    e.target.style.background = "yellow"
+}
